cf457eb6bac03927ba664dd388df87c91190e1fc
Yamat <145124662+yamato080915@users.noreply.github.com>
2025-01-11 01:41:34 +0000

    update

--- main.py
+++ main.py
@@ -1,1 +1,33 @@
+import os, urllib.request as urllib, requests, time
+from tqdm import tqdm
 
+urls = {
+    "updater": "https://github.com/yamato080915/mcserver-updater/archive/refs/heads/main.zip"
+}
+
+folder = input("server folder:")
+if not os.path.isdir(folder):
+    os.mkdir(folder)
+os.chdir(folder)
+
+cwd = os.getcwd()
+
+print("installing mcserver-updater")
+if not os.path.isdir("__cache__"):
+    os.mkdir("__cache__")
+
+bar = tqdm()
+def reporthook(block, bs, size):
+    if size <=0:return
+    bar.total = size
+    bar.update(block*bs if block*bs<=size else size)
+
+urllib.urlretrieve(filename="updater.zip", url=urls["updater"])
+
+proxy = input("Do you want to build a proxy server?(Y/n)")
+if proxy=="Y" or proxy=="y":proxy = True
+else:proxy = False
+if proxy:
+    if not os.path.isdir("proxy"):os.mkdir("proxy")
+    os.chdir("proxy")
+    print("The port used for the proxy server is set to 25564.")
