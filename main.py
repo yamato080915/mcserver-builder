ea2ccf20138129a34229cde8df9acb19ac8074ad
Yamat <145124662+yamato080915@users.noreply.github.com>
2025-01-11 03:09:05 +0000

    update

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import os, urllib.request as urllib, requests, time, shutil
+import os, urllib.request as urllib, requests, time, shutil, json
 from tqdm import tqdm
 
 bar = ""
@@ -23,11 +23,19 @@ if not os.path.isdir("__cache__"):
     os.mkdir("__cache__")
 
 urllib.urlretrieve(filename="__cache__/updater.zip", url=urls["updater"])
+shutil.unpack_archive("__cache__/updater.zip", "./__cache__")
+shutil.move("__cache__/mcserver-updater-main/main.py", "./updater.py")
+shutil.move("__cache__/mcserver-updater-main/README.md", "./")
 
 proxy = input("Do you want to build a proxy server?(Y/n)")
 if proxy=="Y" or proxy=="y":proxy = True
 else:proxy = False
 if proxy:
     if not os.path.isdir("proxy"):os.mkdir("proxy")
-    os.chdir("proxy")
+    jsonData = {"file": "./proxy/server.jar", "software": "velocity", "version": "", "build": 0}
+    with open("proxy.json", "w", encoding="utf-8") as f:
+        json.dump(jsonData, f, indent=4)
+    cmdData = "@echo off\npython updater.py proxy.json\nIF %ERRORLEVEL% == 0 (\n    java -Xmx512M -Xms512M -jar proxy/server.jar --nogui\n    pause\n) ELSE (\n    echo %ERRORLEVEL%\n    pause\n)"
+    with open("./proxy.cmd", "w", encoding="utf-8") as f:
+        f.write(cmdData)
     print("The port used for the proxy server is set to 25564.")
