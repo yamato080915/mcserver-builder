146d220cd885e2966b567e1e4725383cb47b703a
Yamat <145124662+yamato080915@users.noreply.github.com>
2025-01-11 04:32:29 +0000

    update

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-import os, urllib.request as urllib, requests, time, shutil, json
-from tqdm import tqdm
-
-bar = ""
-def reporthook(block, bs, size):
-    if size <=0:return
-    bar.total = size
-    bar.update(block*bs if block*bs<=size else size)
+import os, requests, shutil, json, subprocess
 
 urls = {
     "updater": "https://github.com/yamato080915/mcserver-updater/archive/refs/heads/main.zip"
@@ -22,7 +15,8 @@ print("installing mcserver-updater")
 if not os.path.isdir("__cache__"):
     os.mkdir("__cache__")
 
-urllib.urlretrieve(filename="__cache__/updater.zip", url=urls["updater"])
+with open("__cache__/updater.zip", "wb") as f:
+    f.write(requests.get(urls["updater"]).content)
 shutil.unpack_archive("__cache__/updater.zip", "./__cache__")
 shutil.move("__cache__/mcserver-updater-main/main.py", "./updater.py")
 shutil.move("__cache__/mcserver-updater-main/README.md", "./")
@@ -38,4 +32,5 @@ if proxy:
     cmdData = "@echo off\npython updater.py proxy.json\nIF %ERRORLEVEL% == 0 (\n    java -Xmx512M -Xms512M -jar proxy/server.jar --nogui\n    pause\n) ELSE (\n    echo %ERRORLEVEL%\n    pause\n)"
     with open("./proxy.cmd", "w", encoding="utf-8") as f:
         f.write(cmdData)
+    p = subprocess.call(["python", "updater.py", "proxy.json"])#TODO
     print("The port used for the proxy server is set to 25564.")
