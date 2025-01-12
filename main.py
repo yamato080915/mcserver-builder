8a0938906d4f58a1bbb3d4c79708fc73c6308151
Yamat <145124662+yamato080915@users.noreply.github.com>
2025-01-12 18:35:41 +0900

    update

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 import os, requests, shutil, json, subprocess, sys, yaml, toml
+from tqdm import tqdm
+import urllib.request
 
 urls = {
-    "updater": "https://github.com/yamato080915/mcserver-updater/archive/refs/heads/main.zip"
+    "updater": "https://github.com/yamato080915/mcserver-updater/archive/refs/heads/main.zip", 
+    "jdk": [
+        ["jdk21", "https://download.java.net/java/GA/jdk21.0.2/f2283984656d49d69e91c558476027ac/13/GPL/openjdk-21.0.2_windows-x64_bin.zip", "jdk-21.0.2"], 
+        ["jdk17", "https://download.java.net/java/GA/jdk17.0.2/dfd4a8d0985749f896bed50d7138ee7f/8/GPL/openjdk-17.0.2_windows-x64_bin.zip", "jdk-17.0.2"], 
+        ["jdk16", "https://download.java.net/java/GA/jdk16.0.2/d4a915d82b4c4fbb9bde534da945d746/7/GPL/openjdk-16.0.2_windows-x64_bin.zip", "jdk-16.0.2"], 
+        ["jdk11", "https://download.java.net/java/GA/jdk11/9/GPL/openjdk-11.0.2_windows-x64_bin.zip", "jdk-11.0.2"]
+        ]
 }
 if not os.name == "nt":
     sys.exit("Unsupported OS")
@@ -13,10 +21,26 @@ os.chdir(folder)
 
 cwd = os.getcwd()
 
+if not os.path.isdir("__cache__"):os.mkdir("__cache__")
+
+if not os.path.isdir("jdk"):os.mkdir("jdk")
+
+for i in tqdm(urls["jdk"], total=4, desc="jdk", unit="files"):
+    if not os.path.isdir(f"jdk/{i[0]}"):
+        r = requests.get(i[1], stream=True)
+        with open(f"__cache__/{i[0]}.zip", "wb") as f:
+            for chunk in tqdm(r.iter_content(chunk_size=1024), total=round(int(r.headers.get('content-length', -1))/(1024)), unit="KB", desc=i[2], leave=False):
+                if chunk:
+                    f.write(chunk)
+                    f.flush()
+        shutil.unpack_archive(f"__cache__/{i[0]}.zip", "jdk")
+        shutil.move(f"./jdk/{i[2]}", f"./jdk/{i[0]}")
+
+shutil.rmtree("__cache__")
+os.mkdir("__cache__")
+
 if os.path.isfile("updater.py"):print("updating mcserver-updater")
 else:print("installing mcserver-updater")
-if not os.path.isdir("__cache__"):
-    os.mkdir("__cache__")
 
 with open("__cache__/updater.zip", "wb") as f:
     f.write(requests.get(urls["updater"]).content)
@@ -35,13 +59,13 @@ else:
         jsonData = {"file": "./proxy/server.jar", "software": "velocity", "version": "", "build": 0}
         with open("proxy.json", "w", encoding="utf-8") as f:
             json.dump(jsonData, f, indent=4)
-        cmdData = "@echo off\npy updater.py proxy.json\nIF %ERRORLEVEL% == 0 (\n    cd proxy\n    java -Xmx512M -Xms512M -jar server.jar nogui\n    pause\n) ELSE (\n    echo %ERRORLEVEL%\n    pause\n)"
+        cmdData = "@echo off\npy updater.py proxy.json\nIF %ERRORLEVEL% == 0 (\n    cd proxy\n    java -Xmx512M -Xms512M -jar server.jar nogui\n    pause\n) ELSE (\n    echo %ERRORLEVEL%\n    pause\n)"#TODO java
         with open("./proxy.cmd", "w", encoding="utf-8") as f:
             f.write(cmdData)
         p = subprocess.Popen(["py", "updater.py", "proxy.json"])
         p.wait()
         os.chdir("proxy")
-        p = subprocess.Popen(["java", "-Xmx512M", "-Xms512M", "-jar", "server.jar"], stdin=subprocess.PIPE, shell=True)
+        p = subprocess.Popen(["java", "-Xmx512M", "-Xms512M", "-jar", "server.jar"], stdin=subprocess.PIPE, shell=True)#TODO java
         p.communicate(input="end".encode())
         velocity = toml.load(open("velocity.toml"))
         velocity["player-info-forwarding-mode"] = "modern"
@@ -60,7 +84,7 @@ def add_server():
     jsonData = {"file": f"{servername}/purpur.jar", "software": "purpur", "version": version, "build": 0, "version-up": True if version=="" or version=="latest" else False}
     with open(f"{servername}.json", "w", encoding="utf-8") as f:
         json.dump(jsonData, f, indent=4)
-    cmdData = f"@echo off\npy updater.py {servername}.json\nIF %ERRORLEVEL% == 0 (\n    cd {servername}\n    java -Xmx4G -Xms4G -jar purpur.jar nogui\n    pause\n) ELSE (\n    echo %ERRORLEVEL%\n    pause\n)"
+    cmdData = f"@echo off\npy updater.py {servername}.json\nIF %ERRORLEVEL% == 0 (\n    cd {servername}\n    java -Xmx4G -Xms4G -jar purpur.jar nogui\n    pause\n) ELSE (\n    echo %ERRORLEVEL%\n    pause\n)"#TODO java
     with open(f"./{servername}.cmd", "w", encoding="utf-8") as f:
         f.write(cmdData)
     if not os.path.isdir(servername):os.mkdir(servername)
@@ -69,7 +93,7 @@ def add_server():
     p = subprocess.Popen(["py", "updater.py", f"{servername}.json"])
     p.wait()
     os.chdir(servername)
-    p = subprocess.Popen(["java", "-Xmx4G", "-Xms4G", "-jar", "purpur.jar", "nogui"], stdin=subprocess.PIPE, shell=True)
+    p = subprocess.Popen(["java", "-Xmx4G", "-Xms4G", "-jar", "purpur.jar", "nogui"], stdin=subprocess.PIPE, shell=True)#TODO java
     p.communicate(input="stop".encode())
     if proxy:
         with open("server.properties", "r", encoding="utf-8") as f:
@@ -115,4 +139,3 @@ def add_server():
             f.write(text)
 
 add_server()
-#TODO JDK Version
