7765d54dfc9a594ceda15b82e67cc5ac8cf9b447
Yamat <145124662+yamato080915@users.noreply.github.com>
2025-01-23 20:57:51 +0900

    update

--- gui.py
+++ gui.py
@@ -1,3 +1,13 @@
-import main2
+import tkinter as tk
 
-main = main2.build("server")
+class app(tk.Frame):
+    def __init__(self, master=None, bg="#ffffff"):
+        super().__init__(master, background=bg)
+
+root = tk.Tk()
+root.geometry("600x360")
+
+frame = app(master=root)
+frame.pack(fill=tk.BOTH, expand=True)
+
+root.mainloop()
